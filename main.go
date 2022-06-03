8b84781380d9bb435f7b298f917b860756146e3d
yangcunfeng <yangcunfeng@maka.im>
2022-06-03 22:05:10 +0800

    init

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/idoall/TokenExchangeCommon/commonstock"
+)
 
+func main() {
+	list := commonstock.InitTestKline()
+	stockList := commonstock.NewKDJ(list, 9).Calculation().GetPoints()
+	for _, v := range stockList {
+		fmt.Printf("Time:%s\t Middle:%.5f Up:%.5f Low:%.5f\n", v.Time.Format("2006-01-02 15:04:05"), v.K, v.D, v.J)
+	}
 }
