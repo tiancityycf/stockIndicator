af7c7a293b1e51c6080af8d84093dd330cb1a5bf
yangcunfeng <yangcunfeng@maka.im>
2022-06-03 22:37:03 +0800

    test

--- main.go
+++ main.go
@@ -7,8 +7,17 @@ import (
 
 func main() {
 	list := commonstock.InitStockKline()
-	stockList := commonstock.NewKDJ(list, 9).Calculation().GetPoints()
+
+	//计算新的MACD
+	//stockList := commonstock.NewMACD(list).Calculation().GetPoints()
+	//
+	//for _, v := range stockList {
+	//	fmt.Printf("Time:%s\t DIF:%f DEA:%f MACD %f\n", v.Time.Format("2006-01-02 15:04:05"), v.DIF, v.DEA, v.MACD)
+	//}
+
+	//计算新的KDJ
+	stockList := commonstock.NewKDJ(list, 2).Calculation().GetPoints()
 	for _, v := range stockList {
-		fmt.Printf("Time:%s\t Middle:%.5f Up:%.5f Low:%.5f\n", v.Time.Format("2006-01-02 15:04:05"), v.K, v.D, v.J)
+		fmt.Printf("Time:%s\t K:%.5f D:%.5f J:%.5f\n", v.Time.Format("2006-01-02 15:04:05"), v.K, v.D, v.J)
 	}
 }
